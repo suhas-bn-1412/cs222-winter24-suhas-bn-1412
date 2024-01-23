fbc8f99a5ad2328878e11df5caaf3fe12effdca3
suhas-bn-1412 <suhasbn1412@gmail.com>
2024-01-23 02:51:54 +0000

    Revert "Trying to make util separate library"
    This reverts commit 29bdf36c3c0efc0c6518ee5fc41e352d7450eba1.

--- src/include/util.h
+++ src/include/util.h
@@ -11,17 +11,55 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-namespace PeterDB {
+bool file_exists(const std::string& fileName) {
+    struct stat statbuf;
+    return (0==stat(fileName.c_str(), &statbuf));
+}
 
-bool file_exists(const std::string& fileName);
-void file_delete(const std::string& fileName);
-size_t file_size(const std::string& fileName);
+void file_delete(const std::string& fileName) {
+    remove(fileName.c_str());
+}
 
-void trace(const char* level, const char* format, va_list& args);
-void INFO(const char* format, ...);
-void ERROR(const char* format, ...);
-void WARNING(const char* format, ...);
+size_t file_size(const std::string& fileName) {
+    std::ifstream in(fileName.c_str(),
+                     std::ifstream::in | std::ifstream::binary);
+    in.seekg(0, std::ifstream::end);
+    return in.tellg();
+}
 
+// Sophisticated trace function with format string and variable arguments
+void trace(const char* level, const char* format, va_list& args) {
+    auto len1 = strlen(level);
+    auto len2 = strlen(format);
+
+    char* logstr = new char[len1 + len2 + 2];
+    memcpy(logstr, level, len1);
+    memcpy(logstr+len1, format, len2);
+
+    logstr[len1+len2] = '\n';
+    logstr[len1+len2+1] = '\0';
+    vfprintf(stderr, logstr, args);
+}
+
+void INFO(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    trace("INFO   ", format, args);
+    va_end(args);
+}
+
+void ERROR(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    trace("ERROR  ", format, args);
+    va_end(args);
+}
+
+void WARNING(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    trace("WARNING", format, args);
+    va_end(args);
 }
 
 #endif

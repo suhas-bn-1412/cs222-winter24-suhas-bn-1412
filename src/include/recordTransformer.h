7d764a489cc64aa226c8337d91c730ef66bbed1f
suhas-bn-1412 <suhasbn1412@gmail.com>
2024-01-23 02:51:54 +0000

    Logic to get size of serialized data
    Implemented function to get the size of the serialised data
    so that caller can use this to allocate space for data before
    invoking serialization

--- src/include/recordTransformer.h
+++ src/include/recordTransformer.h
@@ -7,9 +7,9 @@
 namespace PeterDB {
     class RecordTransformer {
         public:
-        static unsigned short getSerializedDataLength(const std::vector<Attribute> &recordDescriptor, const void *recordData);
+        static uint32_t getSerializedDataLength(const std::vector<Attribute> &recordDescriptor, const void *recordData);
 
-        static void serialize(const std::vector<Attribute> &recordDescriptor, void *recordData, void *serializedRecord);
+        static uint32_t serialize(const std::vector<Attribute> &recordDescriptor, void *recordData, void *serializedRecord);
 
         static void deserialize(const std::vector<Attribute> &recordDescriptor, const void *serializedRecord, void* deserializedRecord);
     };
--- src/rbfm/recordTransformer.cc
+++ src/rbfm/recordTransformer.cc
@@ -1,14 +1,76 @@
 #include "src/include/recordTransformer.h"
 
- unsigned short
-PeterDB::RecordTransformer::getSerializedDataLength(const std::vector<Attribute> &recordDescriptor, const void *recordData) {
-    //todo
-    return 0;
+#define ATTR_OFFSET_SZ sizeof(unsigned short)
+#define INT_SZ 4
+#define REAL_SZ 4
+#define VARCHAR_ATTR_SZ 4
+
+uint32_t PeterDB::RecordTransformer::getSerializedDataLength(const std::vector<Attribute> &recordDescriptor, const void *recordData) {
+    return serialize(recordDescriptor, nullptr);
 }
 
-void PeterDB::RecordTransformer::serialize(const std::vector<Attribute> &recordDescriptor, void *recordData,
-                                           void *serializedRecord) {
-    //todo
+inline bool isNthBitSet (unsigned char c, int n) {
+    static unsigned char mask[] = {128, 64, 32, 16, 8, 4, 2, 1};
+    return ((c & mask[n]) != 0);
+}
+
+// given the null flags and the attribute number, returns True if the
+// attribute is defined as null in the flag
+bool isAttrNull(const void *recordData, const uint16_t &attrNum, const uint16_t &totalCount) {
+    uint16_t q = (attrNum-1) / totalCount;
+    uint16_t r = (attrNum-1) % totalCount;
+
+    void *flag_ptr = (void*)((char*)recordData + q);
+    return isNthBitSet((unsigned char)flag_ptr, r);
+}
+
+uint32_t getVarcharAttrSize(void *data) {
+    assert(nullptr != data);
+
+    uint32_t *varcharSz = (uint32_t*)data;
+    return *varcharSz;
+}
+
+uint32_t PeterDB::RecordTransformer::serialize(const std::vector<Attribute> &recordDescriptor, void *recordData,
+                                               void *serializedRecord) {
+    uint16_t attCount = recordDescriptor.size();
+    uint16_t nullFlagSize = attrCount % 8;
+    uint16_t offSetSize = attrCount * ATTR_OFFSET_SZ;
+    uint32_t dataSize = nullFlagSize;
+
+    auto attrNum = 0;
+    bool isNull = false;
+    void *dataPtr = recordData + nullFlagSize;
+
+    for (auto attr : recordDescriptor) {
+        attrNum++;
+        isNull = isAttrNull(recordData, attrNum);
+
+        if (!isNull) {
+            switch (attr.AttrType) {
+                case TypeInt:
+                    dataSize += INT_SZ;
+                    dataPtr = dataPtr + INT_SZ;
+                    if (nullptr == serializedRecord) continue;
+                case TypeReal:
+                    dataSize += REAL_SZ;
+                    dataPtr = dataPtr + REAL_SZ;
+                    if (nullptr == serializedRecord) continue;
+                case TypeVarChar:
+                    uint32_t attrSize = getVarcharAttrSize(dataPtr);
+                    dataSize += attrSize;
+
+                    // we have to move the data pointer past two data
+                    // one is the data which contains size of the varchar attribute
+                    // other is the actual varchar data
+                    dataPtr = dataPtr + (attrSize + VARCHAR_ATTR_SZ);
+                    if (nullptr == serializedRecord) continue;
+                default:
+                    continue;
+            }
+        }
+    }
+    return dataSize;
 }
 
 void

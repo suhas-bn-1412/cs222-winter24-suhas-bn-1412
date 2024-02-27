1430e5d16d700d6f7708c0d9514e92abb099d5f3
iamsu <sujay9626@gmail.com>
2024-02-26 18:22:19 -0800

    Defines pageSerializer, pageDeserializer

---
+++ src/include/pageDeserializer.h
@@ -0,0 +1,17 @@
+#ifndef _page_deserializer_h_
+
+#define _page_deserializer_h_
+
+#include "nonLeafPage.h"
+#include "leafPage.h"
+
+namespace PeterDB {
+    class PageDeserializer {
+    public:
+        void toNonLeafPage(const void *data, NonLeafPage &nonLeafPage);
+
+        void toLeafPage(const void *data, LeafPage &leafPage);
+    };
+}
+
+#endif
---
+++ src/ix/pageSerializer.cc
@@ -0,0 +1,5 @@
+#include "src/include/pageserializer.h"
+
+namespace PeterDB {
+
+}

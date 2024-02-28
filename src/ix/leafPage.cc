1c475d84fdd765ffc9ba522cd06452eaef748f73
iamsu <sujay9626@gmail.com>
2024-02-27 20:12:45 -0800

    Adds nextPgNum and setters to leaf/non-leaf pages

--- src/ix/leafPage.cc
+++ src/ix/leafPage.cc
@@ -2,37 +2,53 @@
 
 namespace PeterDB {
 
-    RidAndKey::RidAndKey(const RID &rid, int intKey) : rid(rid), intKey(intKey) {}
+    RidAndKey::RidAndKey(const RID &rid, int intKey) : _rid(rid), _intKey(intKey) {}
 
-    RidAndKey::RidAndKey(const RID &rid, float floatKey) : rid(rid), floatKey(floatKey) {}
+    RidAndKey::RidAndKey(const RID &rid, float floatKey) : _rid(rid), _floatKey(floatKey) {}
 
-    RidAndKey::RidAndKey(const RID &rid, const std::string &stringKey) : rid(rid), stringKey(stringKey) {}
+    RidAndKey::RidAndKey(const RID &rid, const std::string &stringKey) : _rid(rid), _stringKey(stringKey) {}
 
     const RID &RidAndKey::getRid() const {
-        return rid;
+        return _rid;
     }
 
     int RidAndKey::getIntKey() const {
-        return intKey;
+        return _intKey;
     }
 
     float RidAndKey::getFloatKey() const {
-        return floatKey;
+        return _floatKey;
     }
 
     const std::string &RidAndKey::getStringKey() const {
-        return stringKey;
+        return _stringKey;
     }
 
-    const std::vector<RidAndKey> &LeafPage::getRidAndKey() const {
-        return ridAndKey;
+    const std::vector<RidAndKey> &LeafPage::getRidAndKeyPairs() const {
+        return _ridAndKeyPairs;
     }
 
     const Attribute &LeafPage::getKeyType() const {
-        return keyType;
+        return _keyType;
+    }
+
+    unsigned int LeafPage::getNextPageNum() const {
+        return _nextPageNum;
     }
 
     unsigned int LeafPage::getFreeByteCount() const {
-        return freeByteCount;
+        return _freeByteCount;
+    }
+
+    unsigned int LeafPage::getNumKeys() const{
+        return _ridAndKeyPairs.size();
+    }
+
+    void LeafPage::setNextPageNum(unsigned int nextPageNum) {
+        LeafPage::_nextPageNum = nextPageNum;
+    }
+
+    void LeafPage::setFreeByteCount(unsigned int freeByteCount) {
+        LeafPage::_freeByteCount = freeByteCount;
     }
 }

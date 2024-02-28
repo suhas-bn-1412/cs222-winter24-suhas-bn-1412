98f2a91635fad65f4299b3c963e3f88e55176526
iamsu <sujay9626@gmail.com>
2024-02-27 21:54:13 -0800

    Updates PageDeser definition to inclide isLeafPage

--- src/include/pageDeserializer.h
+++ src/include/pageDeserializer.h
@@ -8,6 +8,8 @@
 namespace PeterDB {
     class PageDeserializer {
     public:
+        bool isLeafPage(const void *data);
+
         void toNonLeafPage(const void *data, NonLeafPage &nonLeafPage);
 
         void toLeafPage(const void *data, LeafPage &leafPage);

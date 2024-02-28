a99bb206e5d84253d363d0b01182235fc1f2411f
iamsu <sujay9626@gmail.com>
2024-02-27 21:00:41 -0800

    Defines and implements pageSerializer

--- src/ix/pageSerializer.cc
+++ src/ix/pageSerializer.cc
@@ -1,12 +1,163 @@
-#include "src/include/pageserializer.h"
+#include "src/include/pageSerializer.h"
+
+typedef char byte;
 
 namespace PeterDB {
 
-    void PageSerializer::toBytes(const NonLeafPage &nonLeafPage, void *data) {
+    class PageMetadataConstants {
+    public:
+        /*
+         * fields are written to/ read from the very end of each page
+        *  this way, any field addition/ deletion/ modification shall not affect
+        * the page's data handling logic
+        *
+        * fields are defined in order of last to first.
+        * _freeByteCount
+        * isLeafPage
+        * _keyType
+        * _nextPageNum
+        * numKeys
+        */
+        static const size_t FREEBYTE_COUNT_VAR_SIZE = sizeof(unsigned int);
+        static const size_t IS_LEAF_PAGE_VAR_SIZE = sizeof(bool);
+        static const size_t KEY_TYPE_VAR_SIZE = sizeof(Attribute);
+        static const size_t NEXT_PAGE_NUM_VAR_SIZE = sizeof(unsigned int);
+        static const size_t NUM_KEYS_VAR_SIZE = sizeof(unsigned int);
 
+        static const unsigned int FREEBYTE_COUNT_OFFSET = PAGE_SIZE - FREEBYTE_COUNT_VAR_SIZE;
+        static const unsigned int IS_LEAF_PAGE_OFFSET = FREEBYTE_COUNT_OFFSET - IS_LEAF_PAGE_VAR_SIZE;
+        static const unsigned int KEY_TYPE_OFFSET = IS_LEAF_PAGE_OFFSET - KEY_TYPE_VAR_SIZE;
+        static const unsigned int NEXT_PAGE_NUM_OFFSET = KEY_TYPE_OFFSET - NEXT_PAGE_NUM_VAR_SIZE;
+        static const unsigned int NUM_KEYS_OFFSET = NEXT_PAGE_NUM_OFFSET - NUM_KEYS_VAR_SIZE;
+    };
+
+    void PageSerializer::toBytes(NonLeafPage nonLeafPage, void *data) {
+        writeFreeByteCount(nonLeafPage.getFreeByteCount(), data);
+        writeIsLeafPage(false, data);
+        writeKeyType(nonLeafPage.getKeyType(), data);
+        writeNextPageNum(nonLeafPage.getNextPageNum(), data);
+        writeNumKeys(nonLeafPage.getNumKeys(), data);
+
+        writePageNumAndKeyPairs(nonLeafPage, data);
     }
 
     void PageSerializer::toBytes(const LeafPage &leafPage, void *data) {
+        writeFreeByteCount(leafPage.getFreeByteCount(), data);
+        writeIsLeafPage(true, data);
+        writeKeyType(leafPage.getKeyType(), data);
+        writeNextPageNum(leafPage.getNextPageNum(), data);
+        writeNumKeys(leafPage.getNumKeys(), data);
 
+        writeKeyAndRidPair(leafPage, data);
+    }
+
+    void PageSerializer::writeFreeByteCount(unsigned int freeByteCount, const void *data) {
+        byte *writePtr = (byte *) data + PageMetadataConstants::FREEBYTE_COUNT_OFFSET;
+        memcpy((void *) writePtr,
+               (void *) &freeByteCount,
+               PageMetadataConstants::FREEBYTE_COUNT_VAR_SIZE);
+    }
+
+    void PageSerializer::writeIsLeafPage(bool isLeafPage, void *data) {
+        byte *writePtr = (byte *) data + PageMetadataConstants::IS_LEAF_PAGE_OFFSET;
+        memcpy((void *) writePtr,
+               (void *) &isLeafPage,
+               PageMetadataConstants::IS_LEAF_PAGE_VAR_SIZE);
+    }
+
+    void PageSerializer::writeKeyType(const Attribute &keyAttribute, const void *data) {
+        byte *writePtr = (byte *) data + PageMetadataConstants::KEY_TYPE_OFFSET;
+        memcpy((void *) writePtr,
+               (void *) &keyAttribute,
+               PageMetadataConstants::KEY_TYPE_VAR_SIZE);
+    }
+
+    void PageSerializer::writeNextPageNum(unsigned int nextPageNum, void *data) {
+        byte *writePtr = (byte *) data + PageMetadataConstants::NEXT_PAGE_NUM_OFFSET;
+        memcpy((void *) writePtr,
+               (void *) &nextPageNum,
+               PageMetadataConstants::NEXT_PAGE_NUM_VAR_SIZE);
+    }
+
+    void PageSerializer::writeNumKeys(unsigned int numKeys, void *data) {
+        byte *writePtr = (byte *) data + PageMetadataConstants::NUM_KEYS_OFFSET;
+        memcpy((void *) writePtr,
+               (void *) &numKeys,
+               PageMetadataConstants::NUM_KEYS_VAR_SIZE);
+    }
+
+    void PageSerializer::writePageNumAndKeyPairs(const NonLeafPage &nonLeafPage, const void *data) {
+        byte *writePtr = (byte *) data;
+        const size_t pageNumSize = sizeof(unsigned int);
+
+        for (const auto &pageNumAndKey: nonLeafPage.getPageNumAndKeys()) {
+            // write _pageNum to file
+            const unsigned int pageNum = pageNumAndKey.getPageNum();
+            memcpy((void *) writePtr, (void *) &pageNum, pageNumSize);
+            writePtr += pageNumSize;
+
+            // write key to file
+            switch (nonLeafPage.getKeyType().type) {
+                case TypeInt: {
+                    const size_t keySize = sizeof(nonLeafPage.getKeyType().length);
+                    const int key = pageNumAndKey.getIntKey();
+                    memcpy((void *) writePtr, (void *) &key, keySize);
+                    writePtr += keySize;
+                    break;
+                }
+
+                case TypeReal: {
+                    const size_t keySize = sizeof(nonLeafPage.getKeyType().length);
+                    const float key = pageNumAndKey.getFloatKey();
+                    memcpy((void *) writePtr, (void *) &key, keySize);
+                    writePtr += keySize;
+                    break;
+                }
+                case TypeVarChar:
+                    //todo
+                    break;
+                default:
+                    ERROR("Illegal Attribute type");
+                    assert(1);
+            }
+        }
+
+
+    }
+
+    void PageSerializer::writeKeyAndRidPair(const LeafPage &leafPage, void *data) {
+        byte *writePtr = (byte *) data;
+        const size_t ridSize = sizeof(RID);
+
+        for (const auto &ridAndKeyPair: leafPage.getRidAndKeyPairs()) {
+            // write RID to file
+            const RID &rid = ridAndKeyPair.getRid();
+            memcpy((void *) writePtr, (void *) &rid, ridSize);
+            writePtr += ridSize;
+
+            // write key to file
+            switch (leafPage.getKeyType().type) {
+                case TypeInt: {
+                    const size_t keySize = sizeof(leafPage.getKeyType().length);
+                    const int key = ridAndKeyPair.getIntKey();
+                    memcpy((void *) writePtr, (void *) &key, keySize);
+                    writePtr += keySize;
+                    break;
+                }
+                case TypeReal: {
+                    const size_t keySize = sizeof(leafPage.getKeyType().length);
+                    const float key = ridAndKeyPair.getFloatKey();
+                    memcpy((void *) writePtr, (void *) &key, keySize);
+                    writePtr += keySize;
+                    break;
+                }
+                case TypeVarChar:
+                    //todo
+                    break;
+                default:
+                    ERROR("Illegal Attribute type");
+                    assert(1);
+            }
+        }
     }
 }

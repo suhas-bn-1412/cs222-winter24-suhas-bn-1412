09a568fe46f4721083c5e236c901ef81e4097616
iamsu <sujay9626@gmail.com>
2024-02-27 20:12:45 -0800

    Adds nextPgNum and setters to leaf/non-leaf pages

--- src/ix/nonLeafPage.cc
+++ src/ix/nonLeafPage.cc
@@ -3,37 +3,53 @@
 namespace PeterDB {
 
     const std::vector<PageNumAndKey> & NonLeafPage::getPageNumAndKeys() const {
-        return pageNumAndKeys;
+        return _pageNumAndKeys;
     }
 
     const Attribute & NonLeafPage::getKeyType() const {
-        return keyType;
+        return _keyType;
     }
 
     unsigned int NonLeafPage::getFreeByteCount() const {
-        return freeByteCount;
+        return _freeByteCount;
     }
 
-    PageNumAndKey::PageNumAndKey(unsigned int pageNum, int intKey) : pageNum(pageNum), intKey(intKey) {}
+    unsigned int NonLeafPage::getNextPageNum() const {
+        return _nextPageNum;
+    }
 
-    PageNumAndKey::PageNumAndKey(unsigned int pageNum, float floatKey) : pageNum(pageNum), floatKey(floatKey) {}
+    unsigned int NonLeafPage::getNumKeys() {
+        return _pageNumAndKeys.size();
+    }
 
-    PageNumAndKey::PageNumAndKey(unsigned int pageNum, const std::string &stringKey) : pageNum(pageNum),
-                                                                                       stringKey(stringKey) {}
+    void NonLeafPage::setNextPageNum(unsigned int nextPageNum) {
+        NonLeafPage::_nextPageNum = nextPageNum;
+    }
+
+    void NonLeafPage::setFreeByteCount(unsigned int freeByteCount) {
+        NonLeafPage::_freeByteCount = freeByteCount;
+    }
+
+    PageNumAndKey::PageNumAndKey(unsigned int pageNum, int intKey) : _pageNum(pageNum), _intKey(intKey) {}
+
+    PageNumAndKey::PageNumAndKey(unsigned int pageNum, float floatKey) : _pageNum(pageNum), _floatKey(floatKey) {}
+
+    PageNumAndKey::PageNumAndKey(unsigned int pageNum, const std::string &stringKey) : _pageNum(pageNum),
+                                                                                       _stringKey(stringKey) {}
 
     unsigned int PageNumAndKey::getPageNum() const {
-        return pageNum;
+        return _pageNum;
     }
 
     int PageNumAndKey::getIntKey() const {
-        return intKey;
+        return _intKey;
     }
 
     float PageNumAndKey::getFloatKey() const {
-        return floatKey;
+        return _floatKey;
     }
 
     const std::string &PageNumAndKey::getStringKey() const {
-        return stringKey;
+        return _stringKey;
     }
 }
